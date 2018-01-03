9c47894825d79917932ff6e85dcdf52226a0584c
Michael Brown <michaelrbrown@gmail.com>
2018-01-03 13:50:11 -0500

    new keystore file. updated for app center. added appcenter packages.

--- Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/BleMvxFormsApp.xaml.cs
+++ Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/BleMvxFormsApp.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MvvmCross.Forms.Core;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using MvvmCross.Forms.Core;
 using MvvmCross.Platform;
 
 namespace CIM.RemoteManager.Core
@@ -13,6 +16,11 @@ namespace CIM.RemoteManager.Core
         protected override void OnStart()
         {
             base.OnStart();
+
+            AppCenter.Start("ios=5a1074f6-8377-43b9-8690-fc377804ee13;" + "uwp={Your UWP App secret here};" +
+                            "android=88ae5767-1223-44bc-a70e-ba476ef09724;",
+                typeof(Analytics), typeof(Crashes));
+
             Mvx.Trace("App Start");
         }
 
