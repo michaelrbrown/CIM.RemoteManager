d67e2e9700d59de5358f3b05ccf0b61663d53be1
Michael Brown <michaelrbrown@gmail.com>
2018-02-08 15:16:02 -0500

    Bug fix for BindableBase

--- Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Models/BindableBase.cs
+++ Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Models/BindableBase.cs
@@ -23,10 +23,14 @@ namespace CIM.RemoteManager.Core.Models
 
         protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
         {
-            if (object.Equals(field, value)) { return false; }
+            if (object.Equals(field, value))
+            {
+                return false;
+            }
 
             field = value;
             this.OnPropertyChanged(propertyName);
             return true;
         }
     }
+}
