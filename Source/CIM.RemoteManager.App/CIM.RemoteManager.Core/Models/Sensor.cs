71f87edaa187c7897039566cee263cfa5a53922c
Michael R Brown <michaelrbrown@gmail.com>
2018-02-09 15:41:26 -0500

    RaisePropertyChanged(() => CharacteristicValue);

--- Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Models/Sensor.cs
+++ Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Models/Sensor.cs
@@ -5,6 +5,7 @@
     /// </summary>
     public class Sensor : BindableBase
     {
+        private double _averageValue;
         public int SensorIndex { get; set; }
         public string SerialNumber { get; set; }
         public string Name { get; set; }
@@ -12,7 +13,17 @@
         public double Scale { get; set; }
         public double Offset { get; set; }
         public int TimeStamp { get; set; }
-        public double AverageValue { get; set; }
+
+        public double AverageValue
+        {
+            get => _averageValue;
+            set
+            {
+                _averageValue = value;
+                OnPropertyChanged(nameof(AverageValue));
+            }
+        }
+
         public double CurrentValue { get; set; }
         public int DisplayConversionCode { get; set; }
         public int DecimalLocation { get; set; }
