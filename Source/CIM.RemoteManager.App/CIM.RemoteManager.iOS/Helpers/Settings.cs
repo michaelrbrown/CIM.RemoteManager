5082d5ac845157dc41ab64000ff48823142a83d0
Michael R Brown <michaelrbrown@gmail.com>
2018-02-09 12:10:22 -0500

    fixed nuget package refs

--- Source/CIM.RemoteManager.App/CIM.RemoteManager.iOS/Helpers/Settings.cs
+++ Source/CIM.RemoteManager.App/CIM.RemoteManager.iOS/Helpers/Settings.cs
@@ -3,7 +3,7 @@
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 
-namespace BLE.Client.iOS.Helpers
+namespace CIM.RemoteManager.iOS.Helpers
 {
 	/// <summary>
 	/// This is the Settings static class that can be used in your Core solution or in any
