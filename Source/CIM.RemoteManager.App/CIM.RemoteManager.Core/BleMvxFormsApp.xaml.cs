1f9f6497ba7b5c3dadf1def8fdd3441ff7e7d059
Michael Brown <michaelrbrown@gmail.com>
2018-02-11 15:08:32 -0500

    Sensor list page updates

--- Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/BleMvxFormsApp.xaml.cs
+++ Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/BleMvxFormsApp.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
-using MvvmCross.Forms.Core;
+﻿using MvvmCross.Forms.Core;
 using MvvmCross.Platform;
 
 namespace CIM.RemoteManager.Core
@@ -16,11 +13,6 @@ namespace CIM.RemoteManager.Core
         protected override void OnStart()
         {
             base.OnStart();
-
-            AppCenter.Start("ios=5a1074f6-8377-43b9-8690-fc377804ee13;" + "uwp={Your UWP App secret here};" +
-                            "android=88ae5767-1223-44bc-a70e-ba476ef09724;",
-                typeof(Analytics), typeof(Crashes));
-            
             Mvx.Trace("App Start");
         }
 
