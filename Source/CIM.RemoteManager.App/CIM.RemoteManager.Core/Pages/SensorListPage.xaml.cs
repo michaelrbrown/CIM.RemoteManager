85dba256e6d4eede7241e01ee650aab4f8ba9d7d
Michael Brown <michaelrbrown@gmail.com>
2018-02-15 15:41:28 -0500

    bug fix

--- Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Pages/SensorListPage.xaml.cs
+++ Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Pages/SensorListPage.xaml.cs
@@ -43,7 +43,7 @@ namespace CIM.RemoteManager.Core.Pages
         /// <param name="e">The event arguments.</param>
         private void OnListViewScrolled(object sender, ScrolledEventArgs args)
         {
-            _userDialogs.Alert($"Offset: {args.ScrollY}", "CIMScan RemoteManager");
+            DisplayAlert("CIMScan RemoteManager", $"Offset: {args.ScrollY}", "OK");
             ScrollOffset.Text = $"Offset = {args.ScrollY}";
         }
 
