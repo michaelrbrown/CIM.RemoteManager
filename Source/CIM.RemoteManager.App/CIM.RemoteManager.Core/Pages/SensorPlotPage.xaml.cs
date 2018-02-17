cb139004221daeb10f7608751a068a41539266f8
Michael Brown <michaelrbrown@gmail.com>
2018-02-17 08:29:50 -0500

    adding plot pages, not tied completely in

--- Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Pages/SensorPlotPage.xaml.cs
+++ Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Pages/SensorPlotPage.xaml.cs
@@ -16,5 +16,19 @@ namespace CIM.RemoteManager.Core.Pages
             
             //SensorBusyIndicator.IsBusy = false;
         }
+
+        /// <summary>
+        /// Handle toggling of sensor updates
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SensorUpdatesSwitchToggled(object sender, ToggledEventArgs e)
+        {
+            // Get instance of SensorListViewModel
+            var sensorListViewModel = (SensorListViewModel)this.BindingContext;
+            // Toggle sensor updates
+            sensorListViewModel.ToggleUpdatesCommand.Execute(null);
+        }
+
     }
 }
