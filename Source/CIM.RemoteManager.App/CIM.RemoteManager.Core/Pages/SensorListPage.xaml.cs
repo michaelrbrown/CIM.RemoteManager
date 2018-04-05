bc89ec9ddb3b292b26b7cf922f50955db2994b52
Michael R Brown <michaelrbrown@gmail.com>
2018-04-05 14:42:21 -0400

    removed invalid images from android project

--- Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Pages/SensorListPage.xaml.cs
+++ Source/CIM.RemoteManager.App/CIM.RemoteManager.Core/Pages/SensorListPage.xaml.cs
@@ -21,7 +21,7 @@ namespace CIM.RemoteManager.Core.Pages
             BindingContext = this;
 
             // Add device settings toolbar icon and handle selection
-            ToolbarItems.Add(new ToolbarItem("Device Settings", "ic_remote-settings.png", () =>
+            ToolbarItems.Add(new ToolbarItem("Device Settings", "ic_RemoteSettings.png", () =>
             {
             }, ToolbarItemOrder.Primary, 0));
         }
